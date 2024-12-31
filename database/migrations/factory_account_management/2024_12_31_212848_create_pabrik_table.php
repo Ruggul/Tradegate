36942fd0ea4c5ce981092393a827f4356e9524f8
Kevinpb86 <133384871+Kevinpb86@users.noreply.github.com>
2024-12-31 21:50:41 +0700

    Create 2024_12_31_212848_create_pabrik_table.php
    update kedua

---
+++ database/migrations/factory_account_management/2024_12_31_212848_create_pabrik_table.php
@@ -0,0 +1,25 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePabrikTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('pabrik', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pabrik');
+            $table->string('kode_pabrik')->unique();
+            $table->text('alamat');
+            $table->string('nomor_telepon');
+            $table->string('email')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('pabrik');
+    }
+} 
