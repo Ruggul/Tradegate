f43fafe0d04a92402f1d6cb9cca65ee92fa1e145
Kevinpb86 <133384871+Kevinpb86@users.noreply.github.com>
2025-01-01 13:36:51 +0700

    Update ketiga.php

--- database/migrations/factory_account_management/2024_12_31_221630_create_karyawan_table.php
+++ database/migrations/factory_account_management/2024_12_31_221630_create_karyawan_table.php
@@ -2,13 +2,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKaryawanPabrikTable extends Migration
+class CreateKaryawanTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    
     public function up()
     {
         Schema::create('karyawan', function (Blueprint $table) {
@@ -23,13 +19,8 @@ class CreateKaryawanPabrikTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('karyawan_pabrik');
+        Schema::dropIfExists('karyawan');
     }
 } 
