4bbf520e31fe846eba4b1f6a3168e355687c81fb
Kevinpb86 <133384871+Kevinpb86@users.noreply.github.com>
2025-01-01 22:21:43 +0700

    update pertama

---
+++ database/migrations/userAccountManagement/2025_01_01_102020_create_user_table.php
@@ -0,0 +1,24 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+} 
