d84bb31b4f4477fcc24a519f22744f2bd97af2b4
Kevinpb86 <133384871+Kevinpb86@users.noreply.github.com>
2025-01-01 13:52:29 +0700

    update perusahaan

---
+++ database/migrations/factory_account_management/2024_12_31_212848_create_perusahaan_table.php
@@ -0,0 +1,25 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerushaanTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('perusahaan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_perusahaan');
+            $table->string('kode_perusahaan')->unique();
+            $table->text('alamat');
+            $table->string('nomor_telepon');
+            $table->string('email')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('perusahaan');
+    }
+} 
